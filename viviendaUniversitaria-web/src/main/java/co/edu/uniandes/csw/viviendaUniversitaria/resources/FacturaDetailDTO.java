4d0fb346dc579f139e6606462bdd69a2d57bacd2
JorgeBejaranoCO <31407800+JorgeBejaranoCO@users.noreply.github.com>
2017-09-26 12:29:02 -0500

    Add files via upload

---
+++ viviendaUniversitaria-web/src/main/java/co/edu/uniandes/csw/viviendaUniversitaria/resources/FacturaDetailDTO.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viviendaUniversitaria.dtos;
+
+import co.edu.uniandes.csw.viviendaUniversitaria.entities.DetalleReservaEntity;
+import co.edu.uniandes.csw.viviendaUniversitaria.entities.DetalleServicioEntity;
+import co.edu.uniandes.csw.viviendaUniversitaria.entities.FacturaEntity;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author je.bejarano10
+ */
+public class FacturaDetailDTO extends FacturaDTO{
+    private EstudianteDTO estudiante;
+    private HospedajeDTO hospedaje;
+    private List <DetalleServicioDTO> detalleServicio;
+    private List <DetalleReservaDTO> detalleReserva;
+
+    public EstudianteDTO getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(EstudianteDTO estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public List<DetalleServicioDTO> getDetalleServicio() {
+        return detalleServicio;
+    }
+
+    public void setDetalleServicio(List<DetalleServicioDTO> detalleServicio) {
+        this.detalleServicio = detalleServicio;
+    }
+
+    public List<DetalleReservaDTO> getDetalleReserva() {
+        return detalleReserva;
+    }
+
+    public void setDetalleReserva(List<DetalleReservaDTO> detalleReserva) {
+        this.detalleReserva = detalleReserva;
+    }
+
+    public HospedajeDTO getHospedaje() {
+        return hospedaje;
+    }
+
+    public void setHospedaje(HospedajeDTO hospedaje) {
+        this.hospedaje = hospedaje;
+    }
+    
+    
+    public FacturaDetailDTO(){
+        super();
+    }
+    public FacturaDetailDTO(FacturaEntity facturaEntity){
+       super(facturaEntity);
+        if(facturaEntity!= null){
+            if(facturaEntity.getEstudiante()!=null){
+           estudiante= new EstudianteDTO(facturaEntity.getEstudiante());
+            }
+            else{
+                facturaEntity.setEstudiante(null);
+            }
+            if(facturaEntity.getHospedaje()!=null){
+           hospedaje=new HospedajeDTO(facturaEntity.getHospedaje());
+            }
+            else{
+            facturaEntity.setHospedaje(null);
+        }
+           detalleServicio= new ArrayList<>();
+           for (DetalleServicioEntity entityDetalleServicio : facturaEntity.getDetalleServicio()) {
+                detalleServicio.add(new DetalleServicioDTO(entityDetalleServicio));
+            }
+           detalleReserva= new ArrayList<>();
+           for (DetalleReservaEntity entityDetalleReserva : facturaEntity.getDetallesReserva()) {
+                detalleReserva.add(new DetalleReservaDTO(entityDetalleReserva));
+            }
+           
+        }
+    }
+    @Override 
+    public FacturaEntity toEntity(){
+        FacturaEntity entity= super.toEntity();
+        if(estudiante!=null){
+            entity.setEstudiante(estudiante.toEntity());
+        }
+        if(hospedaje!=null){
+            entity.setHospedaje(hospedaje.toEntity());
+        }
+        if(detalleServicio!=null){
+            List<DetalleServicioEntity> detalleServicioEntity= new ArrayList<>();
+            for(DetalleServicioDTO dtoDetalleServicio : detalleServicio){
+                detalleServicioEntity.add(dtoDetalleServicio.toEntity());
+            }
+            entity.setDetalleServicio(detalleServicioEntity);     
+        }
+        if(detalleReserva!=null){
+            List<DetalleReservaEntity> detalleReservaEntity= new ArrayList<>();
+            for(DetalleReservaDTO dtoDetalleReserva : detalleReserva){
+                detalleReservaEntity.add(dtoDetalleReserva.toEntity());
+            }
+            entity.setDetallesReserva(detalleReservaEntity);     
+        }
+        return entity;
+    }
+    
+    
+    
+    
+}
