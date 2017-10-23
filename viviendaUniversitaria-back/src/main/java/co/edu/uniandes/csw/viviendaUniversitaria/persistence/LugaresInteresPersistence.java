d3c156b81825391170ccecb3212d1717d3d364f5
jcsanguino10 <30907426+jcsanguino10@users.noreply.github.com>
2017-10-22 19:32:02 -0500

    Persistence funcional con generico de lugares de interes

--- viviendaUniversitaria-back/src/main/java/co/edu/uniandes/csw/viviendaUniversitaria/persistence/LugaresInteresPersistence.java
+++ viviendaUniversitaria-back/src/main/java/co/edu/uniandes/csw/viviendaUniversitaria/persistence/LugaresInteresPersistence.java
@@ -6,48 +6,17 @@
 package co.edu.uniandes.csw.viviendaUniversitaria.persistence;
 
 import co.edu.uniandes.csw.viviendaUniversitaria.entities.LugaresInteresEntity;
-import java.util.List;
-
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 /**
  *
  * @author Juan Camilo Sanguino
  */
 @Stateless
-public class LugaresInteresPersistence {
+public class LugaresInteresPersistence extends GenericPresistence<LugaresInteresEntity>{
 
-    private static final Logger LOGGER = Logger.getLogger(LugaresInteresPersistence.class.getName());
-
-    @PersistenceContext(unitName = "viviendaUniversitariaPU")
-    protected EntityManager em;
-
-    public LugaresInteresEntity findId(Long id) {
-        return em.find(LugaresInteresEntity.class, id);
-    }
-
-    public LugaresInteresEntity create(LugaresInteresEntity entidad) {
-        em.persist(entidad);
-        LOGGER.info("Se creo un nuevo lugar de interes ");
-        return entidad;
-    }
-
-    public LugaresInteresEntity update(LugaresInteresEntity entidad) {
-        LOGGER.info("Actualizando lugar de interes en persistencia");
-        return em.merge(entidad);
-    }
-
-    public void delete(Long id) {
-        em.remove(findId(id));
-    }
-     public List<LugaresInteresEntity> findAll() {
-        LOGGER.info("Consultando todos los lugares de Interes");
-        Query q = em.createQuery("select u from LugaresInteresEntity u");
-        return q.getResultList();
-    }
-
+    public  LugaresInteresPersistence()
+    {
+        super(LugaresInteresEntity.class);
+    }  
 }
