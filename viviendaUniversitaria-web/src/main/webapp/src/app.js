9b3a03f882f41710f15e4eca6488e29808021097
kk.penaranda <kk.penaranda@ISIS2603S4-0048.sis.virtual.uniandes.edu.co>
2017-10-26 01:15:44 -0500

    Inicio Arrendador Front más modificación diseño interfaz

--- viviendaUniversitaria-web/src/main/webapp/src/app.js
+++ viviendaUniversitaria-web/src/main/webapp/src/app.js
@@ -8,7 +8,8 @@
         'origenModule',
         'hospedajeModule',
         'universidadModule',
-        'ubicacionModule'
+        'ubicacionModule',
+        'arrendadorModule'
         
         
     ]);
