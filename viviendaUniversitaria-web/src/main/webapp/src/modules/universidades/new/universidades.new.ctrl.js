f8caa6bb9ec167eca4e4d47972f2ae00d8de1b84
Camilo <ngnjlosblancos@gmail.com>
2017-11-07 06:10:37 -0500

    master normal

--- viviendaUniversitaria-web/src/main/webapp/src/modules/universidades/new/universidades.new.ctrl.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/universidades/new/universidades.new.ctrl.js
@@ -1,18 +1,23 @@
 (function (ng) {
     var mod = ng.module("universidadModule");
+    mod.constant("ubicacionContext","api/ubicaciones");
     mod.constant("universidadesContext", "api/universidades");
-    mod.controller('universidadNewCtrl', ['$scope', '$http', 'universidadesContext', '$state', 'ubicacionContext', '$rootScope',
-        function ($scope, $http, universidadesContext, $state, ubicacionContext, $rootScope) {
-            $rootScope.edit = false;
+    mod.controller('universidadNewCtrl', ['$scope', '$http', 'universidadesContext','$state','ubicacionContext',
+        function ($scope, $http, universidadesContext, $state,ubicacionContext ) {
             $scope.createUniversidad = function () {
-                $http.post(universidadesContext, {
-                    nombre: $scope.universidadNombre,
+                $http.post(ubicacionContext,{
                     direccion: $scope.universidadDireccion,
                     altitud: $scope.universidadAltitud,
                     latitud: $scope.universidadLatitud
+                }).then(function(response){
+                    $scope.ubicacion = response.data;
+                }),
+                $http.post(universidadesContext, {
+                    nombre: $scope.universidadNombre,
+                    ubicacion: $scope.ubicacion.id
                 }).then(function (response) {
                     //universidad created successfully
-                    $state.go('universidadesList', {universidadId: response.data.id}, {reload: true});
+                    $state.go('universidadesList', {}, {reload: true});
                 });
             };
         }
---
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/universidades/ubicacion/ubicaciones.mod.js
@@ -0,0 +1,43 @@
+(function (ng) {
+    var mod = ng.module("ubicacionModule", ['ui.router']);
+    mod.constant("ubicacionesContext", "api/ubicaciones");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/ubicaciones/';
+            $urlRouterProvider.otherwise("/ubicacionesList");
+           
+            $stateProvider.state('ubicaciones', {
+                url: '/ubicaciones',
+                abstract: true,
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'ubicaciones.html',
+                        controller: 'ubicacionCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('ubicacionesList',{
+                url: '/list',
+                parent: 'ubicaciones',
+                views:{
+                    'listView':{
+                        templateUrl: basePath + 'ubicaciones.list.html',
+                        controller: 'ubicacionCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('ubicacionDetail', {
+                url: '/{ubicacionId:int}/detail',
+                parent: 'ubicaciones',
+                param: {
+                    ubicacionId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + 'ubicaciones.detail.html',
+                        controller: 'ubicacionCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
