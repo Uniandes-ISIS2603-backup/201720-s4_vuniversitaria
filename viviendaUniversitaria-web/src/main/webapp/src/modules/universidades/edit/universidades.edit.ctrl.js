faf12dcc382a89909bab274f3c130e2d32d3eede
Camilo <ngnjlosblancos@gmail.com>
2017-11-07 09:31:34 -0500

    cambios x

--- viviendaUniversitaria-web/src/main/webapp/src/modules/universidades/edit/universidades.edit.ctrl.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/universidades/edit/universidades.edit.ctrl.js
@@ -1,28 +1,38 @@
-(function(ng){
-    var mod = ng.module ('universidadModule');
-    mod.constant('universidadesContext','/api/universidades');
-    mod.controller('universidadEditCtrl',['$scope','$state','$http','universidadesContext',
-        function($scope,$state,$http,universidadesContext){
-            
+(function (ng) {
+    var mod = ng.module('universidadModule');
+    mod.constant('universidadesContext', '/api/universidades');
+    mod.constant('ubicacionesContext', '/api/ubicaciones');
+    mod.controller('universidadEditCtrl', ['$scope', '$state', '$http', 'universidadesContext', 'ubicacionesContext', 'ubicacionesEditCtrl',
+        function ($scope, $state, $http, universidadesContext, ubicacionesContext, ubicacionesEditCtrl) {
+
             var idUni = $state.params.universidadId;
-            
-            if(idUni !== undefined) {
-                $http.get(universidadesContext+'/'+ idUni,{}).then(function (response) {    
-                    $scope.universidadActual = response.data;
+
+            if ((idUni !== undefined) && (idUni !== null)) {
+                $http.get(universidadesContext + '/' + idUni).then(function (response) {
+                    $scope.uniActual = response.data;
+                    $scope.ubicacion = response.data.ubicacion.id;
+                });
+            }
+            ;
+
+            $scope.editUniversidad = function () {
+                var nombre = $scope.name;
+                
+                 $http.put(ubicacionesContext + '/' + ubi, {
+                            id: ubi,
+                            direccion: $scope.direccion,
+                            longitud: $scope.longitud,
+                            latitud: $scope.latitud
+                               
+                }),$http.put(universidadesContext + '/' + idUni, {
+                    nombre: $scope.name,
+                    ubicacion: {
+                        id: ubi
+                    }
+                        }).then(function (response) {
+                    $state.go('universidadesList', {}, {reload: true});
                 });
             };
-            $scope.editUniversidad = function(){
-                $http.put(universidadesContext +'/'+ idUni,{
-                    
-                    nombre: $scope.nombre,
-                    direccion: $scope.direccion,
-                    latitud: $scope.latitud,
-                    altitud:$scope.altitud
-                    
-                }).then(function(response){
-                    $state.go('universidadesList',{},{reload:true});
-                });
-            }; 
         }
     ]);
 })(window.angular);
