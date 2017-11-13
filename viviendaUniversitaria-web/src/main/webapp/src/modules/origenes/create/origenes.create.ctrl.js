8c4c430124b9c496729f5595aed5e2e32257798b
a.eslava <a.eslava@ISIS2603S4-0025.sis.virtual.uniandes.edu.co>
2017-11-13 14:44:23 -0500

    siguen sin servir

--- viviendaUniversitaria-web/src/main/webapp/src/modules/origenes/create/origenes.create.ctrl.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/origenes/create/origenes.create.ctrl.js
@@ -1,14 +1,14 @@
 (function (ng) {
     var mod = ng.module("origenModule");
     mod.constant("origenesContext", "api/origenes");
-    mod.controller('origenCreateCtrl', ['$scope', '$http', '$state', 'origenesContext', '$rootScope',
-        function ($scope, $http, origenesContext, $state,$rootScope) {
+    mod.controller('origenCreateCtrl', ['$scope', '$http', 'estudiantesContext', '$state', 'origenesContext', '$rootScope',
+        function ($scope, $http, origenesContext, $state, booksContext, $rootScope) {
             $rootScope.edit = false;
             $scope.createOrigen = function () {
                 $http.post(origenesContext, {
                     name: $scope.origenName
                 }).then(function (response) {
-                    $state.go('origenesList',{origenId: response.data.id},{reload: true});
+                    $state.go('origenesList',{origenesId: response.data.id},{reload: true});
                 });
             };
         }
