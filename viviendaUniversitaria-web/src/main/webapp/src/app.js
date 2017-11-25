b89f98cbc4330dc58f16dbef2010694a10ca0bd2
ms.osorio <ms.osorio@ISIS2603S4-0028.sis.virtual.uniandes.edu.co>
2017-11-24 21:56:45 -0500

    Mapa siriviendo, error en el bower. Se corrige con git config --global url."https://".insteadOf git://

--- viviendaUniversitaria-web/src/main/webapp/src/app.js
+++ viviendaUniversitaria-web/src/main/webapp/src/app.js
@@ -3,6 +3,7 @@
         // External dependencies
         'ui.router',
         'ui.bootstrap',
+         'uiGmapgoogle-maps',
         // Internal modules dependencies  
         'reservaModule',
         'facturaModule',
@@ -14,8 +15,13 @@
         'lugaresInteresModule',
         'hospedajeModule'
     ]);
-    // Resuelve problemas de las promesas
-    app.config(['$qProvider', function ($qProvider) {
+    //Resuelve problemas
+    app.config(['$qProvider','uiGmapGoogleMapApiProvider', function ($qProvider,uiGmapGoogleMapApiProvider) {
+               uiGmapGoogleMapApiProvider.configure({
+                 key: 'AIzaSyAGX2fd-BcdoHwrTtZbtrcxNM9j7WUrxSY',
+                 v: '3.20', //defaults to latest 3.X anyhow
+                 libraries: 'weather,geometry,visualization'
+        });
             $qProvider.errorOnUnhandledRejections(false);
         }]);
 })(window.angular);
