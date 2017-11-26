c9d40fa904dbcac9f646fad4cfd1c9636dff1c5f
ws.duarte <ws.duarte@ISIS2603S4-0021.sis.virtual.uniandes.edu.co>
2017-11-26 18:21:47 -0500

    Correcion de bug

--- viviendaUniversitaria-web/src/main/webapp/src/modules/hospedaje/regla/regla.update.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/hospedaje/regla/regla.update.js
@@ -4,16 +4,16 @@
     mod.controller('reglaUpdate', ['$scope', '$http', 'hospedajeContext', '$state',
         function ($scope, $http, hospedajeContext, $state) {
             $scope.reglaActiva = {};
-            
-                $http.get(hospedajeContext + '/' + $state.params.idHospedaje + '/reglas/'+$state.params.idRegla).then(function (response) {
-                    $scope.reglaActiva =  response.data;
-                });
-            
+            this.idHospedaje = $state.params.idHospedaje;
+
+            $http.get(hospedajeContext + '/' + $state.params.idHospedaje + '/reglas/' + $state.params.idRegla).then(function (response) {
+                $scope.reglaActiva = response.data;
+            });
+
             $scope.updateRegla = function () {
-                $http.put(hospedajeContext + '/' + $state.params.idHospedaje + '/reglas/'+$state.params.idRegla, {
-                    regla:$scope.regla
+                $http.put(hospedajeContext + '/' + $state.params.idHospedaje + '/reglas/' + $state.params.idRegla, {
+                    regla: $scope.regla
                 }).then(function (response) {
-                    alert(response.data.regla);
                     $state.go('hospedajeEspecifico', {idHospedaje: $state.params.idHospedaje});
                 });
             };
@@ -22,5 +22,5 @@
 }
 )(angular);
 
-            
+
             
