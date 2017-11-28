8ff7581914b105b62b8b5a75f66411e790390539
ws.duarte <ws.duarte@ISIS2603S4-0021.sis.virtual.uniandes.edu.co>
2017-11-28 13:56:44 -0500

    Login y alertas dulces

---
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/login/logout.ctrl.js
@@ -0,0 +1,31 @@
+(function (ng) {
+    var mod = ng.module("hospedajeModule");
+    mod.controller('logoutControlador', ['$scope', '$http', 'loginContext', '$state', '$rootScope',
+        function ($scope, $http, loginContext, $state, $rootScope) {
+
+            $scope.cerrarSesion = function () {
+                $rootScope.rolActivo = "No registrado";
+                $rootScope.idActivo = "";
+                $scope.estado = true;
+            };
+
+            swal({
+                title: '¿Está seguro que desea cerrar sesion?',
+                type: 'info',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Cerrar sesion'
+            }).then((result) => {
+                if (result.value) {
+                    $scope.cerrarSesion();
+                }
+                $state.go('home');
+            });
+
+
+        }
+    ]);
+}
+)(angular);
+
