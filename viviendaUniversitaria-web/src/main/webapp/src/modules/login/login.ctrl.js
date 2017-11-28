2c8b5ec45ff557ad75c40b79835f3e7e2e4c719f
ws.duarte <ws.duarte@ISIS2603S4-0021.sis.virtual.uniandes.edu.co>
2017-11-28 00:48:36 -0500

    Adicion del id

--- viviendaUniversitaria-web/src/main/webapp/src/modules/login/login.ctrl.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/login/login.ctrl.js
@@ -5,17 +5,23 @@
         function ($scope, $http, loginContext, $state, $rootScope) {
 
             $rootScope.rolActivo = "No registrado";
+            $rootScope.idActivo = "";
             $scope.estado = true;
 
             $scope.valudarUsuario = function (nombreUsuario, contrasenia) {
-                $http.get('resources/data/ussers.json').then(function (response) {
-                    $scope.estado = false;
+                $scope.estado = false;
+                $rootScope.idActivo = "";
                     $rootScope.rolActivo = "No registrado";
+                $http.get('resources/data/ussers.json').then(function (response) {
+                    
                     var data = response.data.admins;
                     for (i = 0; i < data.length; i++) {
                         if (data[i].nombreUsuario === nombreUsuario) {
-                            $rootScope.rolActivo = "Administrador";
+                            if(data[i].contrasenia === contrasenia) {
+                                $rootScope.rolActivo = "Administrador";
+                                $rootScope.idActivo = "";
                             $scope.estado = true;
+                            }
                         }
                     }
                 });
@@ -26,6 +32,7 @@
                         if (data.rol !== 'Error') {
                             if (data.contrasenia === contrasenia) {
                                 $rootScope.rolActivo = data.rol;
+                                $rootScope.idActivo = data.id;
                                 $scope.estado = true;
                             } else {
                                 $rootScope.rolActivo = "No registrado";
