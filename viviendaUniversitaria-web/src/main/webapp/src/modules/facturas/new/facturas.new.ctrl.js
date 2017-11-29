bb084d12bf9569801ed341b37228f29e993d0ac0
JorgeBejaranoCO <31407800+JorgeBejaranoCO@users.noreply.github.com>
2017-11-29 14:29:59 -0500

    Hice magia con factura.

--- viviendaUniversitaria-web/src/main/webapp/src/modules/facturas/new/facturas.new.ctrl.js
+++ viviendaUniversitaria-web/src/main/webapp/src/modules/facturas/new/facturas.new.ctrl.js
@@ -8,14 +8,21 @@
                 $scope.reservasRecords = response.data;
             });
            $scope.createFactura = function () {
-            $http.post(facturasContext + '/123/1', {
-                id : Math.floor((Math.random() * 100) + 1),
+            $http.post(facturasContext + '/'+$scope.reservaSeleccionada.estudiante.id+'/'+$scope.reservaSeleccionada.hospedaje.id, {
+                id : Math.floor((Math.random() * 1000) + 1),
                 fecha: $scope.fecha,
                 iva: $scope.iva,
                 total:$scope.total
             });
         };
-  
+                $scope.calcularTotal= function(){
+                    var inicio=$scope.reservaSeleccionada.fechaInicio;
+                    var fin=$scope.reservaSeleccionada.fechaFin;
+                    var inicioDef=inicio.replace("-","");
+                    var finDef=fin.replace("-","");
+                    return (finDef-inicioDef)*19532;
+                   
+                };
     }
     ]);
 }
